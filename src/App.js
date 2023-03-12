edfe82afbdad87194fba9787e960fd91561a44ac
lhs0698 <yohs0698@naver.com>
2023-03-12 17:47:38 +0900

    MyButton 공용컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -6,32 +6,41 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
-function App() {
+// 공용컴포넌트
+import MyButton from "./components/myButton";
 
-  //<img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} alt="" />가 작동을 하지 않는다면 이렇게 설정해서 사용할 수 있음 
+function App() {
+  //<img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} alt="" />가 작동을 하지 않는다면 이렇게 설정해서 사용할 수 있음
   // const env = process.env;
   // env.PUBLIC_URL = env.PUBLIC_URL || "";
-
-
-
 
   return (
     <BrowserRouter>
       <div className="App">
         <h2>App.js</h2>
         {/* process.env.PUBLIC_URL 은 pulic 폴더로 바로 갈수 있는 경로 */}
-        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} alt="" />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} alt="" />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} alt="" />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} alt="" />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} alt="" />
+        {/* <img src={process.env.PUBLIC_URL + "/assets/emotion1.png"} alt="" /> */}
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼클릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼클릭")}
+          type={"negative"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼클릭")}
+          type={"default"}
+        />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/new" element={<New />}></Route>
           <Route path="/edit" element={<Edit />}></Route>
           <Route path="/diary/:id" element={<Diary />}></Route>
         </Routes>
-        
       </div>
     </BrowserRouter>
   );
---
+++ src/components/myButton.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const MyButton = ({ text, type, onClick }) => {
+
+  // btnType 설정 이유 
+  // props로 받는 type안에 'positive or negativ가 있는지 확인하고 없다면 type을 default로 강제시킨다
+  // includes : 배열이 특정 요소를 포함하고 있는지 판별하는 매서드
+  const btnType = ['positive', 'negative'].includes(type) ? type : 'default'  
+    
+  return (
+    <button onClick={onClick} className={['MyButton', `MyButton_${btnType}`].join(" ")}>
+      {text}
+    </button>
+  );
+};
+
+MyButton.defaultProps = {
+    type: 'default'
+}
+
+export default MyButton;
+
