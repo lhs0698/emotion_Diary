b46c52d8a35cba1d21d7f2a01940f46a84eaa47f
lhs0698 <yohs0698@naver.com>
2023-03-19 17:20:15 +0900

    emotion 필터링

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,9 +1,16 @@
 import { useState } from "react";
 
+
 const sortOptionList = [
   { value: "lastest", name: "최신순" },
   { value: "oldest", name: "오래된 순" },
 ];
+
+const filterOptionList = [
+    { value : "all", name: "전부다"},
+    { value : "good", name: "좋은 감정만"},
+    { value : "bad", name: "안좋은 감정만"},
+]
 
 const ControlMenu = ({value, onChange, optionList}) => {
   return (
@@ -19,19 +26,32 @@ const ControlMenu = ({value, onChange, optionList}) => {
 
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState("lastest");
+  const [filter, setFilter] = useState('all');
 
   const getProcessedDiaryList = () => {
+
+    const filterCallBack = (item) => {
+        if(filter === 'good') {
+            return item.emotion <= 3
+        } else {
+            return item.emotion > 3
+        }
+    }
+
     const compare = (a,b) => {
-        // console.log(a)
-        console.log(b)
         if(sortType === 'lastest') {
             return parseInt(b.date) - parseInt(a.date)
         } else {
             return parseInt(a.date) - parseInt(b.date)
         }
     }
+
     const copyList = JSON.parse(JSON.stringify(diaryList));
-    const sortedList = copyList.sort(compare)
+
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it));
+    console.log(filteredList)
+    const sortedList = filteredList.sort(compare)
     return sortedList
   } 
 
@@ -41,9 +61,16 @@ const DiaryList = ({ diaryList }) => {
         value={sortType}
         onChange={setSortType}
         optionList={sortOptionList}
+      /> 
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
       />
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <div key={it.id}>
+            {it.content} {it.emotion}    
+        </div>
       ))}
     </div>
   );
