0fd1af4abb87de832196d7fb929c626a804f206f
lhs0698 <yohs0698@naver.com>
2023-04-08 18:52:50 +0900

    emotion icon css 추가, strDate 추가

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,6 +1,16 @@
-const DiaryItem = () => {
+const DiaryItem = ({id, emotion, content, date}) => {
+
+    const strDate = new Date(parseInt(date)).toLocaleString();
+
+
     return (
-        <div className="DiaryItem"></div>
+        <div className="DiaryItem">
+            <div className={["emotion_img_wrapper", `emotion_img_wrapper_${emotion}`].join(" ")}>
+                <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`}/>
+            </div>
+            <div className="diary_date">{strDate}</div>
+            <div className="diray_content_prview"></div>
+        </div>
     )
 }
 
