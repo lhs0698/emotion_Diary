f5a29062f20e95c419e1bea9d68ba0927b9e9a47
lhs0698 <yohs0698@naver.com>
2023-04-09 18:57:53 +0900

    New Component 시작

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,22 @@
+import MyHearder from "../components/MyHearder";
+import MyButton from "../components/MyButton";
+
+import { useNavigate } from "react-router-dom";
+
+
 const New = () => {
+
+    const navigate = useNavigate();
+
     return (
         <div>
-            <h1>New</h1>
-            <div>이곳은 New</div>
+            <MyHearder 
+            headText={"새 일기쓰기"}
+            leftChild={<MyButton text={'< 뒤로가기'} onClick={()=> navigate(-1)}/>}
+            />
+            <div>
+                <section></section>
+            </div>
         </div>
     )
 }
