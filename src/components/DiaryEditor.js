2bd3cb16fe7f397155c576445aa619c1d04e9466
lhs0698 <yohs0698@naver.com>
2023-04-17 22:15:16 +0900

    Diary 상세 페이지

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -7,55 +7,15 @@ import { DiaryDispatchContext } from "./../App.js";
 
 import EmotionItem from "./EmotionItem";
 
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: "완전 좋음",
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: "좋음",
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: "그럭저럭",
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: "나쁨",
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: "끔찍함",
-  },
-];
+import { getStringDate } from "../util/data";
+import { emotionList } from "../util/emotion";
 
 // const getStringDate = (date) => {
 //   return date.toISOString().slice(0, 10);
 //   // toISOString 메서드는 단순화한 확장 ISO형식의 문자열은 반환한다
 //   // YYYY-MM-DDTHH로 표현한다
 // };
-const getStringDate = (date) => {
-  let year = date.getFullYear();
 
-  let month = date.getMonth() + 1;
-
-  let day = date.getDate();
-
-  if (month < 10) {
-    month = `0${month}`;
-  }
-
-  if (day < 10) {
-    day = `0${day}`;
-  }
-  return `${year}-${month}-${day}`;
-};
 
 const DiaryEditor = ({isEdit, originData}) => {
   const contentRef = useRef();
